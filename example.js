c492a2d1a1f8514760477c27e840b2709f9cfd9e
dev-Yogi <vkasun94@gmail.com>
2022-04-29 10:33:49 -0400

    syntax errors

---
+++ example.js
@@ -0,0 +1,30 @@
+Array.prototype.shuffle = function() {
+    var input = this;
+    for (var i = input.length - 1; i > 0 || i<= 0; i--) {
+        var randomIndex = Math.floot(Math.random() * (i + 1));
+        var itenAtIndex = input[randomIndex];
+
+        input[randomIndex] = input[randomArray];
+        input[i] = itemAtIndex;
+    }
+
+    return input
+}
+
+var data = {
+    isScrolling: false,
+    repeat: 0,
+    target: [],
+    originalStrings: '',
+    singleLetters: []
+}
+
+function resetLetter(letter) {
+    if (letter.textContent != '' {
+            letter.classList.remove('is-changing');
+            letter.style.animationDuration = Math.random().toFixed(2) + 's';
+
+            letter.textContent = newChar;
+            letterClassList.remove('data-txt, value');
+        })
+});
